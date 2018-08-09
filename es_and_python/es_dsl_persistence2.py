d44eced21d89c53cd0d750ec66839db0a454a679
clip <binderclip2014@gmail.com>
2018-08-09 20:07:34 +0800

    bulk create, bulk delete

--- es_and_python/es_dsl_persistence2.py
+++ es_and_python/es_dsl_persistence2.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 from elasticsearch_dsl import Document, Date, Nested, Boolean, \
     analyzer, InnerDoc, Completion, Keyword, Text, connections
 
 
 HOSTS = ['localhost:9200']
 CONNECTION_ALIAS = 'dev'
+INDEX = 'blog'
 es_client = Elasticsearch(HOSTS, timeout=5)
 connections.add_connection(CONNECTION_ALIAS, es_client)
 
@@ -17,6 +19,7 @@ html_strip = analyzer('html_strip',
     char_filter=["html_strip"]
 )
 
+
 class Comment(InnerDoc):
     author = Text(fields={'raw': Keyword()})
     content = Text(analyzer='snowball')
@@ -24,6 +27,7 @@ class Comment(InnerDoc):
 
     def age(self):
         return datetime.now() - self.created_at
+
 
 class Post(Document):
     title = Text()
@@ -38,7 +42,7 @@ class Post(Document):
     comments = Nested(Comment)
 
     class Index:
-        name = 'blog'
+        name = INDEX
         using = CONNECTION_ALIAS
 
     def add_comment(self, author, content):
@@ -50,50 +54,103 @@ class Post(Document):
         return super().save(** kwargs)
 
 
-def main():
-    # init
-    # Post.init()
+def init():
+    print('=== init ===')
+    Post.init()
 
-    # create and save
-    # post = Post(meta={'id': 25}, title='Hello ES!')
-    # post.created_at = datetime.now()
-    # post.published = False
-    # post.category = 'es dev tutorial'
-    # post.add_comment(author='clip', content='good')
-    # post.save()
 
-    # update
-    # post = Post.get(id=25)
-    # post.update(published=True)
+def create_and_save():
+    print('=== create_and_save ===')
+    post = Post(meta={'id': 25}, title='Hello ES!')
+    post.created_at = datetime.now()
+    post.published = False
+    post.category = 'es dev tutorial'
+    post.add_comment(author='clip', content='good')
+    post.save()
 
-    # get
-    # post = Post.get(id=25)
-    # print(post.comments, post.published)
 
-    # get none
-    # Post.get(id='not-in-es')  # elasticsearch.exceptions.NotFoundError: TransportError(404, '{"_index":"blog","_type":"doc","_id":"not-in-es","found":false}')
-    # post = Post.get(id='not-in-es', ignore=404)
-    # print(post)
+def update():
+    print('=== update ===')
+    post = Post.get(id=25)
+    post.update(published=True)
 
-    # get multi
-    # posts = Post.mget([25, 26])
-    # print(posts)
 
-    # delete
-    # post = Post.get(id=25)
-    # post.delete()
+def get():
+    print('=== get ===')
+    post = Post.get(id=25)
+    print(post.comments, post.published)
 
-    # search
+def get_none():
+    print('=== get_none ===')
+    Post.get(id='not-in-es')  # elasticsearch.exceptions.NotFoundError: TransportError(404, '{"_index":"blog","_type":"doc","_id":"not-in-es","found":false}')
+    post = Post.get(id='not-in-es', ignore=404)
+    print(post)
+
+
+def get_multi():
+    print('=== get_multi ===')
+    posts = Post.mget([25, 26])
+    print(posts)
+
+
+def delete():
+    print('=== delete ===')
+    post = Post.get(id=25)
+    post.delete()
+
+
+def bulk_create():
+    print('=== bulk_create ===')
+    posts = [
+        Post(meta={'id': 10}, title='Get Started with ES 01'),
+        Post(meta={'id': 11}, title='Get Started with ES 02'),
+    ]
+    actions = (doc.to_dict(include_meta=True) for doc in posts)
+    # es_client.bulk(actions)  # not work
+    bulk(es_client, actions)
+
+
+def bulk_delete():
+    print('=== bulk_delete ===')
+    post_ids = [10, 11]
+    actions = []
+    for post_id in post_ids:
+        actions.append({
+            "_op_type": "delete",
+            "_index": Post._index._name,
+            "_type": Post._doc_type.mapping.properties._name,
+            "_id": str(post_id)
+        })
+    errors = bulk(es_client, actions=actions)
+    return not errors
+
+
+def search():
+    print('=== search ===')
     s = Post.search()
     s = s[0:1]
     s = s.filter('term', published=True).query('match', title='Hello')
     results = s.execute()
     for post in results:
         print(post.meta.score, post.title)
-
     print(f'total: {results.hits.total}')
+
+
+def main():
+    # init()
+    # create_and_save()
+    # update()
+    # get()
+    # get_none()
+    # get_multi()
+    # delete()
+    # bulk_create()
+    # bulk_delete()
+    search()
+    print(Post._doc_type.mapping.properties._name)
 
 
 if __name__ == '__main__':
     main()
 
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html
