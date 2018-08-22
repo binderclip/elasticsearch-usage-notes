b2b4b5e139b934684618cc6c08248a9622ecc9ec
clip <binderclip2014@gmail.com>
2018-08-22 21:24:39 +0800

    python test analyzer

--- es_and_python/es_py_demo2.py
+++ es_and_python/es_py_demo2.py
@@ -14,15 +14,17 @@ def insert_star_war_people_data(es: Elasticsearch):
         r = requests.get('http://swapi.co/api/people/' + str(i))
         es.index(index=INDEX_NAME, doc_type='people', id=i, body=json.loads(r.content))
         i = i + 1
-    print(i)
+        print(i)
 
 
 def count_documents(es: Elasticsearch):
+    print('=== count_documents ===')
     r = es.count(index=INDEX_NAME)
     print(r)
 
 
 def have_a_search(es: Elasticsearch):
+    print('=== have_a_search ===')
     r = es.search(index=INDEX_NAME, body={"query": {"match": {'name':'Darth Vader'}}})
     print(r)
 
@@ -30,11 +32,18 @@ def have_a_search(es: Elasticsearch):
     print(r)
 
 
+def test_analyzer(es: Elasticsearch):
+    print('=== test_analyzer ===')
+    r = es.indices.analyze(index=INDEX_NAME, body={"analyzer": "standard", "text": "big watermelon"})
+    print(r)
+
+
 def main():
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
     # insert_star_war_people_data(es)
     # count_documents(es)
-    have_a_search(es)
+    # have_a_search(es)
+    test_analyzer(es)
 
 
 if __name__ == '__main__':
